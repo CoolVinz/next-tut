29876cd64d1e5d44818cc05631978bf4abe2b51f
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-09-03 22:50:31 +0700

    fix size in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="flex items-center justify-center vertical-center">
+      <LoginForm />
+    </div>
+  );
 }
