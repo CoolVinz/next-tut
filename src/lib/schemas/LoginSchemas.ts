06cb868feb35c78f3b9bb8d8c35df8cb0050ce3f
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-09-04 11:31:35 +0700

    fix form validate

--- src/lib/schemas/LoginSchemas.ts
+++ src/lib/schemas/LoginSchemas.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email({ message: "คุณกรอก Email ไม่ถูกต้อง" }),
   password: z.string().min(6, {
     message: "กรุณา กรอกรหัสผ่านมากกว่า 6 ตัวอักษร",
   }),
